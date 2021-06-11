9bd17412938a0535944694934f9f70ee178ca4cf
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-11 10:29:41 -0600

    wip

--- data/migrations/20210611160041_initial-migration.js
+++ data/migrations/20210611160041_initial-migration.js
@@ -2,16 +2,32 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', table => {
-      table.increments()
+      table.increments('project_id')
+      table.string('project_name', 200).notNullable()
+      table.string('project_description')
+      table.integer('project_completed').defaultTo(0)
     })
     .createTable('resources', table => {
-      table.increments()
+      table.increments('resource_id')
+      table.string('resource_name', 200).notNullable().unique()
+      table.string('resource_description')
     })
     .createTable('tasks', table => {
-      table.increments()
+      table.increments('task_id')
+      table.string('task_description').notNullable()
+      table.string('task_notes')
+      table.integer('task_completed').defaultTo(0)
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     })
     .createTable('project_resources', table => {
-      table.increments()
+      table.increments('project_resource_id')
+
     })
 };
 
